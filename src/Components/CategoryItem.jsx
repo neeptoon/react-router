340e0929b0814dfafd80fab6a9d645ce691456f2
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-04-11 11:48:08 +0300

    add Home Page

---
+++ src/Components/CategoryItem.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+
+export default function CategoryItem (props) {
+	const {idCategory, strCategory, strCategoryThumb, strCategoryDescription} = props.category;
+	return (
+		<div className="card">
+			<div className="card-image">
+				<img src={strCategoryThumb}/>
+			</div>
+			<div className="card-content">
+				<span className="card-title">{strCategory}</span>
+				<p>{strCategoryDescription.slice(0, 60)}...</p>
+			</div>
+			<div className="card-action">
+				<Link to={`/category/${idCategory}`} className="btn">Смотреть категории</Link>
+			</div>
+		</div>
+	)
+}
